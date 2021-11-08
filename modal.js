bcb458674f56e66b4fe6ca1045c894944f8339cb
tom-costa <tomcosta@hotmail.co.uk>
2021-11-08 21:52:59 +0000

    Final videos and songs added, minor refactoring updates

---
+++ modal.js
@@ -0,0 +1,14 @@
+const modalBtn = document.querySelector('.modal-btn');
+const modalOverlay = document.querySelector('.modal-overlay');
+const closeBtn = document.querySelector('.close-btn');
+
+
+modalBtn.addEventListener('click', function() {
+    modalOverlay.classList.add('open-modal')
+    console.log("hi")
+});
+
+closeBtn.addEventListener('click', function() {
+    modalOverlay.classList.remove('open-modal')
+});
+
